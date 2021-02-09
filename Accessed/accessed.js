065b4f01075e4eeb9078cb8ca1c8d591b7695e91
FelipeMarqueStefano <65095646+FelipeMarqueStefano@users.noreply.github.com>
2021-02-09 19:04:05 -0300

    Add files via upload

---
+++ Accessed/accessed.js
@@ -0,0 +1,26 @@
+function acesso(form) {
+
+    form.txtEmail.value = form.txtEmail.value.toLowerCase()
+    form.txtPassword.value = form.txtPassword.value.toLowerCase()
+
+    if (form.txtEmail.value == "felipeopdeop@gmail.com" && form.txtPassword.value == "stefano123@") {
+        location = "../Accessed/accessed.html"}
+    else {
+        form.txtEmail.value = ""
+        form.txtPassword.value = ""
+        alert("Login or password error!")
+    }
+
+    var email = txtEmail
+    var senha = txtPassword
+
+    if (email == txtEmail && senha == txtPassword) {
+        alert('Email e senha capturado.')
+        confirm('Esse é o email recebido: ' + email.value)
+    }
+
+    else {
+        alert('Email não capturado.' + 'Senha não capturada')
+    }
+
+}
