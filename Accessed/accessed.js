32dd412242777d1b9364d20026493ea421313874
FelipeMarqueStefano <65095646+FelipeMarqueStefano@users.noreply.github.com>
2021-02-09 19:23:42 -0300

    Update accessed.js

--- Accessed/accessed.js
+++ Accessed/accessed.js
@@ -3,7 +3,7 @@ function acesso(form) {
     form.txtEmail.value = form.txtEmail.value.toLowerCase()
     form.txtPassword.value = form.txtPassword.value.toLowerCase()
 
-    if (form.txtEmail.value == "felipeopdeop@gmail.com" && form.txtPassword.value == "stefano123@") {
+    if (form.txtEmail.value == "pudim@deleite.com" && form.txtPassword.value == "pudim123@") {
         location = "../Accessed/accessed.html"}
     else {
         form.txtEmail.value = ""
